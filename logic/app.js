1e56c2922226c875a8f205a69589037e7112fef0
Muchtar <arefi.muchtar@gmail.com>
2022-07-06 21:41:42 +0200

    feat:darkening of colors with gradually incrementing blackness (total of 10% blackness)

--- logic/app.js
+++ logic/app.js
@@ -4,12 +4,14 @@ let sliderValue = document.querySelector(".sliderValue");
 let applyButton = document.querySelector(".applyButton");
 let clearButton = document.querySelector(".clearButton");
 let theInput = document.getElementById("kb_selected_color");
-let colorCircle=document.querySelector(".colorCircle");
+let colorCircle = document.querySelector(".colorCircle");
+let darkening = true;
 let theColor = theInput.value;
 let hoveredGridfields;
 let mouseclickInField = false;
-let penColor = "blue";
-colorCircle.style.backgroundColor=penColor;
+let penColor = "#0000FF";
+colorCircle.style.backgroundColor = penColor;
+
 
 
 window.onload = loadGrid(slider.value);
@@ -55,6 +57,7 @@ sketchGrid.addEventListener("click", () => {
 function enableColoring(hoveredGridfields, mouseclickInField) {
     console.log(mouseclickInField)
     hoveredGridfields.forEach((field) => {
+        
         if (mouseclickInField) {
             field.addEventListener("mouseover", eventHandler)
         } else {
@@ -69,16 +72,49 @@ function enableColoring(hoveredGridfields, mouseclickInField) {
 theInput.addEventListener("input", function () {
     document.getElementById("hex").innerHTML = theInput.value;
     penColor = theInput.value;
-    colorCircle.style.backgroundColor=penColor;
+    colorCircle.style.backgroundColor = penColor;
 }, false);
 
 
 function eventHandler(e) {
-    e.target.style.backgroundColor = penColor;
+    if (!(e.target.style.backgroundColor == "white") && darkening) {
+      e.target.style.backgroundColor=`${shadeColor(e.target.style.backgroundColor)}`;
+       
+    } else {
+        e.target.style.backgroundColor = penColor
+    }
+
 }
 
 
+function shadeColor(color) {
+    color = color.substring(4, color.length-1)
+         .replace(/ /g, '')
+         .split(',');
+         let R = (parseInt(color[0]-26)<=0?0:parseInt(color[0]-26));
+         let G = (parseInt(color[1]-26)<=0?0:parseInt(color[1]-26));
+         let B = (parseInt(color[2]-26)<=0?0:parseInt(color[2]-26));
+    return RGBToHex(`rgb(${R}, ${G}, ${B})`);
+}
 
 
+function RGBToHex(rgb) {
+    // Choose correct separator
+    let sep = rgb.indexOf(",") > -1 ? "," : " ";
+    // Turn "rgb(r,g,b)" into [r,g,b]
+    rgb = rgb.substr(4).split(")")[0].split(sep);
 
-/**https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors */
+    let r = (+rgb[0]).toString(16),
+        g = (+rgb[1]).toString(16),
+        b = (+rgb[2]).toString(16);
+
+    if (r.length == 1)
+        r = "0" + r;
+    if (g.length == 1)
+        g = "0" + g;
+    if (b.length == 1)
+        b = "0" + b;
+    return "#" + r + g + b;
+}
+
+
